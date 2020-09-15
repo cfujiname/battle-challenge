1197364e18866e6005e1fba1696b74dff4c9f6ad
cristina <c.fujiname@gmail.com>
2020-09-15 17:44:37 +0100

    Add web_helpers.rb

--- spec/features/get_name_spec.rb
+++ spec/features/get_name_spec.rb
@@ -1,9 +1,7 @@
+
 feature 'Enter 2 player names' do
   scenario  "players enter name into forms"do
-    visit('/') # creating context
-    fill_in :player_1_name, with: 'Bob' #fill in form field
-    fill_in :player_2_name, with: 'Builder'
-    click_button 'Submit' # create a button
+    sign_in_and_play
     expect(page).to have_content 'Bob vs. Builder'
   end
 end
